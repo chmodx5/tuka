98b6f91f165a91cc796144440a716182758c7782
chmodx5 <chmodx5@gmail.com>
2022-02-23 18:35:22 +0300

    navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -4,13 +4,14 @@ import { useRouter } from "next/router";
 import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
 import { FaTimes } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import Navmenu from "./Navmenu";
 
 const Navbar = () => {
   const router = useRouter();
   const [mobilemenu, setMobilemenu] = useState(false);
 
   return (
-    <nav className=" shadow-lg fixed bg-white w-full z-50 ">
+    <nav className=" shadow-lg fixed bg-white dark:bg-gray-900 dark:text-white w-full z-50 ">
       <div className="container mx-auto px-2 sm:px-6 md:px-0 ">
         <div className=" h-20 flex justify-between items-center">
           {/* page logo */}
@@ -18,14 +19,17 @@ const Navbar = () => {
             <Link href="/">
               <a>
                 <h1 className="font-bold text-2xl uppercase">
-                  <span className=" text-primary">Nya</span>ngumi
+                  <span className=" text-primary">Tu</span>ka
                 </h1>
               </a>
             </Link>
           </div>
 
           <div className="md:flex items-center hidden ">
-            <div className=" flex space-x-2 "></div>
+            <div className=" flex space-x-2 ">
+              <button>account</button>
+              <button>cart</button>
+            </div>
             <ThemeSwitch />
           </div>
 
@@ -58,6 +62,38 @@ const Navbar = () => {
               </ul>
             </div>
           )}
+        </div>
+      </div>
+      <div className="bg-gray-900">
+        <div className="container mx-auto">
+          <div className=" text-white">
+            <ul className="flex gap-2 items-center">
+              <li>
+                <Link href="/">
+                  <a href="" className=" font-semibold uppercase px-2">
+                    home
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Navmenu />
+              </li>
+              <li>
+                <Link href="/">
+                  <a href="" className=" font-semibold uppercase  px-2">
+                    FAQ
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a href="" className=" font-semibold uppercase  px-2">
+                    About
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
