81097c77f11d9b348ca1b985efb2c1428d899798
chmodx5 <chmodx5@gmail.com>
2022-02-24 09:32:48 +0300

    grouped products like trending items fetured items etc

---
+++ components/GroupedProducts/GroupedProducts.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Product from "../Product/Product";
+
+const GroupedProducts = () => {
+  return (
+    <section className="py-10">
+      <div className="mb-10">
+        <h2 className="font-semibold text-center text-3xl">
+          This weeks highlights
+        </h2>
+      </div>
+      <div className="grid grid-cols-5 gap-6">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </section>
+  );
+};
+
+export default GroupedProducts;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,6 @@
+import GroupedProducts from "../components/GroupedProducts/GroupedProducts";
 import Product from "../components/Product/Product";
 
 export default function Home() {
-  return (
-    <div className="grid grid-cols-5 gap-6">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-    </div>
-  );
+  return <GroupedProducts />;
 }
