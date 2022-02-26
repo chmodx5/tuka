9560f488a69ac874c4575f6174dc8a75887ee70b
chmodx5 <chmodx5@gmail.com>
2022-02-26 13:58:57 +0300

    adding the single slide component

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "next/image";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import Slide from "./Slide";
 
 const Hero = () => {
   return (
@@ -51,90 +52,13 @@ const Hero = () => {
         slidesToSlide={1}
         swipeable
       >
-        <div className="grid grid-cols-12 h-full ">
-          <div className="col-span-6 flex items-center px-6 ">
-            <div className="w-3/4">
-              <p className="text-red-500 font-bold mb-5">
-                🔥 Hot Deal This Week
-              </p>
-              <h2 className="text-4xl font-bold mb-10">
-                Bluetooth Wireless Earbuds Headphone
-              </h2>
-              <Link passHref href="/">
-                <button className="app__btn-outline">shop now</button>
-              </Link>
-            </div>
-          </div>
-          <div className="col-span-6 h-full">
-            <div className="h-96 flex justify-end ">
-              <div className=" relative h-full aspect-square ">
-                <Image
-                  src="/collection-image.png"
-                  alt="alt"
-                  className=" object-center object-cover "
-                  layout="fill"
-                  priority
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-12 h-full ">
-          <div className="col-span-6 flex items-center px-6 ">
-            <div className="w-3/4">
-              <p className="text-red-500 font-bold mb-5">
-                🔥 Hot Deal This Week
-              </p>
-              <h2 className="text-4xl font-bold mb-10">
-                Bluetooth Wireless Earbuds Headphone
-              </h2>
-              <Link passHref href="/">
-                <button className="app__btn-outline">shop now</button>
-              </Link>
-            </div>
-          </div>
-          <div className="col-span-6 h-full">
-            <div className="h-96 flex justify-end ">
-              <div className=" relative h-full aspect-square ">
-                <Image
-                  src="/collection-image.png"
-                  alt="alt"
-                  className=" object-center object-cover "
-                  layout="fill"
-                  priority
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-12 h-full ">
-          <div className="col-span-6 flex items-center px-6 ">
-            <div className="w-3/4">
-              <p className="text-red-500 font-bold mb-5">
-                🔥 Hot Deal This Week
-              </p>
-              <h2 className="text-4xl font-bold mb-10">
-                Bluetooth Wireless Earbuds Headphone
-              </h2>
-              <Link passHref href="/">
-                <button className="app__btn-outline">shop now</button>
-              </Link>
-            </div>
-          </div>
-          <div className="col-span-6 h-full">
-            <div className="h-96 flex justify-end ">
-              <div className=" relative h-full aspect-square ">
-                <Image
-                  src="/collection-image.png"
-                  alt="alt"
-                  className=" object-center object-cover "
-                  layout="fill"
-                  priority
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        <Slide />
+        <Slide />
+        <Slide />
+        <Slide />
+        <Slide />
+        <Slide />
+        <Slide />
       </Carousel>
     </section>
   );
