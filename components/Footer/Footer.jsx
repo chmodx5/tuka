c4c95cf18636f96d081f1bc2eb3c898ed0a6e639
chmodx5 <chmodx5@gmail.com>
2022-02-28 23:25:55 +0300

    styling the footer link

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className="">Footer</div>;
+  return (
+    <footer className="bg-gray-200 py-10 w-full">
+      <div className="container mx-auto">
+        <div
+          className="grid grid-cols-1 sm
+        :grid-cols-2 md:grid-cols-4"
+        >
+          <div>
+            <h3 className="footer__group-heading">Information</h3>
+            <ul>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">About Us</a>
+                </Link>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
