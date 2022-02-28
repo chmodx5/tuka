393a4e3adb0c4976c687413cd364e4147aa5e235
chmodx5 <chmodx5@gmail.com>
2022-02-28 23:34:15 +0300

    adding link names to the footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -10,7 +10,52 @@ const Footer = () => {
         :grid-cols-2 md:grid-cols-4"
         >
           <div>
+            <h3 className="footer__group-heading">Company</h3>
+            <ul>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">My account</a>
+                </Link>
+              </li>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Wish list</a>
+                </Link>
+              </li>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Product compare</a>
+                </Link>
+              </li>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">About Us</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
             <h3 className="footer__group-heading">Information</h3>
+            <ul>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Register</a>
+                </Link>
+              </li>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Login</a>
+                </Link>
+              </li>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Cart</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="footer__group-heading">Contact</h3>
             <ul>
               <li>
                 <Link passHref href="/about">
@@ -18,13 +63,39 @@ const Footer = () => {
                 </Link>
               </li>
               <li>
-                <a href="#">About Us</a>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Contact Us</a>
+                </Link>
               </li>
               <li>
-                <a href="#">About Us</a>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Faq</a>
+                </Link>
               </li>
               <li>
-                <a href="#">About Us</a>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Blog</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="footer__group-heading">Follow</h3>
+            <ul>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Facebook</a>
+                </Link>
+              </li>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">instagram</a>
+                </Link>
+              </li>
+              <li>
+                <Link passHref href="/about">
+                  <a className="footer_group-link">Twitter</a>
+                </Link>
               </li>
             </ul>
           </div>
