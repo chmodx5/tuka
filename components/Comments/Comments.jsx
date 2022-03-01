a1f48bf4febd2256ef033a85eb5e3afcd04a79ea
chmodx5 <chmodx5@gmail.com>
2022-03-01 17:59:43 +0300

    rating

---
+++ components/Comments/Comments.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Rating from "../Rating/AppRating";
+
+const Comments = () => {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold mb-10">Reviews</h3>
+      <Rating />
+    </div>
+  );
+};
+
+export default Comments;
