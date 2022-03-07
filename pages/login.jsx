23145b901e8370f280e4af9acb1d744e4790d855
chmodx5 <chmodx5@gmail.com>
2022-03-07 19:32:36 +0300

    login

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,21 +1,22 @@
+import axios from "axios";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
-  const onSubmit = (e) => {
+  const submit = async (e) => {
     e.preventDefault();
-    console.log("email", email, "password", password);
+    axios
+      .post("http://localhost:3000/auth/login", {
+        username: name,
+        password: password,
+      })
+      .then((response) => {
+        router.push("/Login");
+        console.log(response);
+      });
   };
 
   return (
@@ -24,16 +25,16 @@ export default function Login() {
         <div className="  col-span-6">
           <h1 className=" mb-10 text-2xl font-bold">Login</h1>
 
-          <form action="" onSubmit={onSubmit}>
+          <form action="" onSubmit={submit}>
             <div className="grid grid-cols-12 gap-4">
               <label className="col-span-3" htmlFor="">
                 Email
               </label>
               <input
                 type="text"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 id=""
                 className="form__input col-span-9"
               />
@@ -47,7 +48,7 @@ export default function Login() {
                 type="password"
                 name="password"
                 value={password}
-                onChange={handleChange}
+                onChange={(e) => setPassword(e.target.value)}
                 className="form__input col-span-9"
                 id=""
               />
