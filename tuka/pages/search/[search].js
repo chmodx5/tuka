6d5c27441ba447b75ba71184d7c06556fe74048c
chmodx5 <chmodx5@gmail.com>
2022-03-11 08:48:41 +0300

    price input range

--- tuka/pages/search/[search].js
+++ tuka/pages/search/[search].js
@@ -9,17 +9,16 @@ import {
   ListItemAvatar,
   Avatar,
   ListItemText,
+  Slider,
 } from "@mui/material";
 
 const Search = () => {
   const router = useRouter();
-  const [checked, setChecked] = useState([1]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState(null);
   const [category, setCategory] = useState(null);
-
   const [selectedCategories, setSelectedCategories] = useState([]);
-  const [checkedCategory, setCheckedCategory] = useState();
+  const [priceRangeSlider, setPriceRangeSlider] = useState([5, 80]);
   //getting a list of all categories
   useEffect(() => {
     axios.get(`http://localhost:3999/categories/`).then((response) => {
@@ -43,6 +42,7 @@ const Search = () => {
     }
   }, [search]);
 
+  //handling the category check input
   const handleCheckEvent = (e, categoryid) => {
     let somearray = [...selectedCategories];
     if (e.target.checked === true) {
@@ -58,13 +58,33 @@ const Search = () => {
     }
   };
 
+  const handleChange = (event, newValue, activeThumb) => {
+    const minDistance = 10;
+
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 100 - minDistance);
+        setPriceRangeSlider([clamped, clamped + minDistance]);
+      } else {
+        const clamped = Math.max(newValue[1], minDistance);
+        setPriceRangeSlider([clamped - minDistance, clamped]);
+      }
+    } else {
+      setPriceRangeSlider(newValue);
+    }
+  };
+
   useEffect(() => {}, []);
 
   return (
     <section className="grid grid-cols-12 gap-6 app__section">
-      <div className="col-span-4 space-y-6">
+      <div className="col-span-4 md:col-span-4 lg:col-span-3 space-y-6 ">
         {/* START filter by category */}
-        <div className="bg-gray-100 shadow-md py-4 rounded">
+        <div className="app__card">
           <h4 className="px-3 font-semibold">Category</h4>
           <List dense>
             {category && category.length > 0 && (
@@ -100,8 +120,45 @@ const Search = () => {
         </div>
         {/* END filter by category */}
 
-        {/* START filter by price range */}
+        {/* START filter by price range *with slider* */}
+        <div className="px-2 app__card ">
+          <div className="flex justify-between">
+            <h4 className=" font-semibold">Price range</h4>
+            <button className="app__btn text-sm capitalize text-gray-400 hover:text-black">
+              apply
+            </button>
+          </div>
 
+          <div className=" ">
+            <Slider
+              value={priceRangeSlider}
+              onChange={handleChange}
+              valueLabelDisplay="auto"
+              disableSwap
+              // color="primary"
+              colorPrimary
+              max={100000}
+              // getAriaValueText={valuetext}
+            />
+            <div className="flex justify-between">
+              <span>
+                <input
+                  className=" dark:bg-gray-900 outline-gray-300 focus:outline-primary outline-2 outline rounded-sm inline-block px-1 py-1 w-full text-xs"
+                  type="number"
+                  value={priceRangeSlider[0]}
+                />
+              </span>
+              <span className="px-1">-</span>
+              <span>
+                <input
+                  className="dark:bg-gray-900 outline-gray-300 focus:outline-primary outline-2 outline rounded-sm inline-block px-1 py-1 w-full text-xs"
+                  type="number"
+                  value={priceRangeSlider[1]}
+                />
+              </span>
+            </div>
+          </div>
+        </div>
         {/* END filter by price range */}
       </div>
       <div className="col-span-8">
